c8eb34c26d6cb621f3a7247969c09dbd27a8e382
Dokimos25 <122038004+Dokimos25@users.noreply.github.com>
2023-10-29 00:23:35 +0400

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework.permissions import BasePermission, 
-SAFE_METHODS, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (BasePermission, 
+SAFE_METHODS, IsAuthenticatedOrReadOnly)
 
 
 class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
