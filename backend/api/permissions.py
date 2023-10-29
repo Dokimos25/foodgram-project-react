75014171fdb5d8c7bfb75588c85c6013aceb3146
Dokimos25 <122038004+Dokimos25@users.noreply.github.com>
2023-10-29 20:56:46 +0400

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -11,9 +11,5 @@ class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
 
 
 class IsSameUser(BasePermission):
-    # def has_permission(self, request, view):
-    #     return request.user.is_authenticated
-    # or request.method in SAFE_METHODS
-
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj == request.user
